eec40a74fac896a502ee284999471f28f2481989
Kasanderh <Kasanderh@gmail.com>
2020-09-13 15:27:27 +0200

    Added icon and title to application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,13 +12,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Tabulator");
         primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("/style/icon.png"));
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    // Event handlers
 }
